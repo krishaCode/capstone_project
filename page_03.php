f60c06bfcb6358724a7ef6f9f84dff99a3d5c7e1
Nimbinu <164753328+Nimbinu@users.noreply.github.com>
2025-06-02 20:56:11 +0530

    Add Firebase configuration

--- page_03.php
+++ page_03.php
@@ -6,6 +6,19 @@
   <link rel="stylesheet" href="rack-item.css">
   <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
   <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+  <script>
+  const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
+    projectId: "YOUR_PROJECT",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
+  };
+  firebase.initializeApp(firebaseConfig);
+</script>
+
 
 </head>
 <body>
