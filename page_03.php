d0211055bbac8029e0b2b17f067764ac4479900b
Nimbinu <164753328+Nimbinu@users.noreply.github.com>
2025-06-03 10:01:18 +0530

    Add page layout and action buttons

--- page_03.php
+++ page_03.php
@@ -18,9 +18,27 @@
   };
   firebase.initializeApp(firebaseConfig);
 </script>
-
-
 </head>
 <body>
+  <div class="container">
+  <div class="card">
+    <table>
+      <thead>
+        <tr>
+          <th>Rack Number</th>
+          <th>Sensor ID</th>
+          <th>Product</th>
+          <th>Product Weight (Kg)</th>
+          <th>Weight on Rack (Kg)</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody id="product-table-body"></tbody>
+    </table>
+  </div>
+</div>
+<a href="dashboard.php"><button class="back-button">←</button></a>
+<a href="existing-p.php"><button class="add-button">+</button></a>
+
 </body>
 </html>
