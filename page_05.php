4958acae396ded0de0d6231030d2adc68531e486
Nimbinu <164753328+Nimbinu@users.noreply.github.com>
2025-06-05 12:24:56 +0530

    Add form layout

--- page_05.php
+++ page_05.php
@@ -22,5 +22,17 @@
 
 </head>
 <body>
+  <div class="form-container">
+  <h2>Existing Product</h2>
+  <form id="rackForm">
+    <input type="text" id="userId" placeholder="User ID" required>
+    <input type="text" id="sensorId" placeholder="Sensor ID" required>
+    <input type="text" id="rackNo" placeholder="Rack No" required>
+    <input type="text" id="product_id" placeholder="Product ID" required>
+    <input type="number" id="weight_level" placeholder="Weight Level (Kg)" required>
+    <button type="submit">Submit</button>
+  </form>
+</div>
+
 </body>
 </html>
