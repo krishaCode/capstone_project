e3594f68ba0f86756dc62d8efe39745d74db96ce
Withyen <wijayakumaranwithyen@gmail.com>
2025-07-01 23:28:01 +0530

    firebase config added

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAE7m1Ggpae_NGCD81PZdMFwaW7nwMasiQ",
+  authDomain: "scale-8ca5f.firebaseapp.com",
+  databaseURL: "https://scale-8ca5f-default-rtdb.firebaseio.com",
+  projectId: "scale-8ca5f",
+  storageBucket: "scale-8ca5f.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abcdef123456"
+};
+
+firebase.initializeApp(firebaseConfig);
